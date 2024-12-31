55d639d3902bf91972613e9b44e0cbd3344a652b
IsaacVic-Dark <isaacvwarui@gmail.com>
2024-12-31 19:16:37 +0300

    Second wave updates

---
+++ js/admin.js
@@ -0,0 +1,6 @@
+document.getElementById('orders').addEventListener('click', function() {
+    const element = document.getElementById('element');
+    const dashboard_section = document.getElementById('dashboard_section');
+    element.style.display = 'block'; 
+    dashboard_section.style.display = 'none'; 
+});
